4d4d676833b0239609b6a49527ecc6fd9e52abf3
Vladimir <Vladimir@y-solutions.ru>
2022-08-18 14:50:43 +0300

    Initial commit

---
+++ src/main/java/sky/pro/exeptionsprosky/ExeptionsProSkyApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.exeptionsprosky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeptionsProSkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExeptionsProSkyApplication.class, args);
+	}
+
+}
